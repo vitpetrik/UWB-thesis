d4c0347382ba20b63b0e8b359fea14f0678e0ffc
vitpetrik <vit.petrik@gmail.com>
2023-05-07 13:26:48 +0200

    PHY dataflow figure

--- uwb.jl
+++ uwb.jl
@@ -1,54 +1,68 @@
-# Define some parameters
-Tc = 250e-9 # chip duration in seconds
-Tdsym = 1e-6 # symbol duration in seconds
-Nc = round(Int, Tdsym/Tc) # number of chips per symbol
-Np = 2 # number of pulses per burst
-fc = 0.5e9 # carrier frequency in Hz
-fs = 20e9 # sampling frequency in Hz
-Ts = 1/fs # sampling period in seconds
-Ns = round(Int, Tc/Ts) # number of samples per chip
+# Plot the waveform
+using Plots
+gr()
 
-# Define a function to generate a single UWB pulse
-function pulse(t)
-    return exp(-t^2/(Tc/4)^2) * cos(2*pi*fc*t)
-end
+
+default_plots = (
+    fontfamily="computer modern",
+    lims = :round,
+    minorgrid=true,
+    legend=false,
+    color_palette=:lighttest,
+    widen=true,
+)
+
 
 β = 0.5
 Tp = 2e-9
 r(t) = 4*β*(cos((1+β)*pi*t/Tp)+sin((1-β)*pi*t/Tp)/(4*β*(t/Tp)))/(pi*sqrt(Tp)*(1-(4*β*t/Tp)^2))
 
+f_carrier=6.5e9
+T_dsym = 128.21e-9
+T_bpm = T_dsym/2
+T_c = Tp
+N_burst = 8
+N_hop = 2
+N_cpb = 1
+N_c = 64
+f_s = 40e9 # sampling frequency in Hz
+T_s = 1/f_s # sampling period in seconds
+N_s = round(Int, T_c/T_s) # number of samples per chip
+T_burst = T_dsym/N_burst
+
+time_offset = -T_dsym/2
+
 # Define a function to generate a BPM-BPSK symbol for two bits
-function bpmbpsk(bits)
+function bpmbpsk(bits, position, len)
     # Check if the input is valid
     if length(bits) != 2 || !all(x -> x in [0, 1], bits)
         error("Invalid input bits")
     end
     
     # Allocate an array for the symbol waveform
-    sym = zeros(Float64, Ns*Nc)
-    
-    # Determine the burst position and phase from the bits
-    pos = bits[1] == 0 ? 1 : Nc÷2 + 1 # first or second half of the symbol
-    phase = bits[2] == 0 ? 1 : -1 # positive or negative polarity
-    
-    # Generate the burst waveform by adding Np pulses
-    burst = zeros(Float64, 0)
-    for i in 1:Np
-        t = range((i-1)*Tc, length=Ns, step=Ts) # time vector for one pulse
-        append!(burst, pulse.(t))
-        # burst += pulse.(t) # add the pulse to the burst
+    t = range(time_offset, length=len, step=T_s) # time vector for one pulse
+    wave = zeros(Float64, len)
+
+    for (i, time) in enumerate(t)
+        chips = range(1, N_cpb)
+
+        burst = 0
+
+        for chip in chips
+            pulse = r(time - bits[1]*T_bpm - chip*T_c - position*T_dsym)
+            if isnan(pulse)
+                pulse = r(time - bits[1]*T_bpm - chip*T_c - 1e-20 - position*T_dsym)
+            end
+            burst += pulse
+        end
+
+        wave[i] = (1-2*bits[2])*burst
     end
-    
-    # Apply the phase to the burst
-    burst *= phase
-    
-    # Insert the burst into the symbol at the correct position
-    sym[(pos-1)*Ns+1 : (pos+Np-1)*Ns] = burst
-    
-    return sym
+
+    print(maximum(abs.(wave)))
+    return wave
 end
 
-# Define a function to generate a BPM-BPSK waveform for a bit sequence
 function bpmbpsk_waveform(bits)
     # Check if the input is valid
     if length(bits) % 2 != 0 || !all(x -> x in [0, 1], bits)
@@ -56,27 +70,31 @@ function bpmbpsk_waveform(bits)
     end
     
     # Allocate an array for the waveform
-    wave = zeros(Float64, Ns*Nc*length(bits)÷2)
+    len = round(Int, abs(N_s*N_c+time_offset/T_dsym) + N_s*N_c*length(bits)÷2)
+    wave = zeros(Float64, len)
     
     # Loop over each pair of bits and generate a symbol
     for i in 1:2:length(bits)
-        sym = bpmbpsk(bits[i:i+1]) # generate a symbol for two bits
-        wave[(i-1)*Ns*Nc÷2+1 : (i+1)*Ns*Nc÷2] = sym # insert the symbol into the waveform
+        wave += bpmbpsk(bits[i:i+1], (i-1)÷2, len) # generate a symbol for two bits
+        # wave[(i-1)*N_s*N_c÷2+1 : (i+1)*N_s*N_c÷2] = sym # insert the symbol into the waveform
     end
+
+    wave /= maximum(abs.(wave))
+
+    carrier = cos.(2*pi*f_carrier*t)
+    # wave = wave .* carrier
     
     return wave
 end
 
-# Example usage: generate a BPM-BPSK waveform for 8 bits
-bits = [0, 1, 1, 0, 0, 1, 0, 1]
+bits = [0, 0, 0, 1, 1, 0, 1, 1]
 wave = bpmbpsk_waveform(bits)
 
-# Plot the waveform
-using Plots
-pyplot()
-
-t = range(0, length=length(wave), step=Tdsym/(Ns*Nc)) # time vector for the waveform
-pl = plot(t*1e9, wave, xlabel="Time (ns)", ylabel="Amplitude", title="BPM-BPSK waveform")
+t = range(time_offset, length=length(wave), step=T_dsym/(N_s*N_c)) # time vector for the waveform
+pl = plot(t*1e9, wave;default_plots..., legend=true, xlabel="Time (ns)", ylabel="Amplitude", title="BPM-BPSK waveform")
+vline!(pl, [0, 128.21, 256.42, 384.63, 512.84], label="Symbols", color=:black, linestyle=:dash)
+vline!(pl, [T_bpm, 3*T_bpm, 5*T_bpm, 7*T_bpm]*1e9, label="Bursts", color=:blue, linestyle=:dash)
+annotate!(pl, 0, 0.5, text("0 0", :center))
 display(pl)
 
 readline()
